496e9b6fbdc966fcaa44c56989962282db0bd28c
febrasko <105153916+febrasko@users.noreply.github.com>
2022-09-15 15:13:27 -0300

    consulta

---
+++ HelloWorld/Consulta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace HelloWorld
+{
+    public partial class Consulta : Form
+    {
+        public Consulta()
+        {
+            InitializeComponent();
+        }
+
+        private void Consulta_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
